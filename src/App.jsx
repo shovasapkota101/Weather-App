f0e47fafc23ea79cb8b6f94f8872142b41352e6d
shova <shovasapkota101@gmail.com>
2026-03-10 01:19:01 +0545

    Add WeatherCard component to display city weather info

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import axios from 'axios';
 import './App.css'
 import SearchBar from './Components/SearchBar'
+import WeatherCard from './Components/WeatherCard';
 
 function App() {
   const [weather, setWeather] = useState(null);
@@ -9,23 +10,25 @@ function App() {
   const [error, setError] = useState("");
 
   const API_KEY =import.meta.env.VITE_API_KEY;
-  const API_URL = `http://maps.openweathermap.org/maps/2.0/weather/{op}/{z}/{x}/{y}?appid={API key}`;
+  const API_URL = `https://api.openweathermap.org/data/2.5/weather`;
 
   const fetchWeather = async (city) =>{
     setLoading(true);
     setError('');
      try{
-      const url = `${API_URL}?q=${city}& units=metric&appid=${API_KEY}`;
+      const url = `${API_URL}?q=${city}&units=metric&appid=${API_KEY}`;
       const response = await axios.get(url);
       console.log(response.data);
       setWeather(response.data);
     } catch (err) {
       if (err.response && err.response.status === 404) {
-        setError('City not found. Please try agaon.')
+        setError('City not found. Please try again.')
       } else {
         setError("An error occurred. Please try again later.")
       }
       setWeather(null);
+    } finally {
+      setLoading(false);
     }
   }
 
@@ -35,6 +38,9 @@ function App() {
        <div className='bg-black/90 text-white rounded-lg shadow-lg p-8 max-w-md w-full'>
          <h1 className='text-3xl font-bold text-center mb-6'>Weather App</h1>
          <SearchBar fetchWeather={fetchWeather}/>
+         {loading && <p className='text-center mt-4'>Loading...</p>}
+         {error && <p className='text-red-500 text-center mt-4'>{error}</p>}
+         {weather && <WeatherCard weather = {weather}/>}
        </div>
       </div>
     </>
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react'
 
-const SearchBar = (fetchWeather) => {
+const SearchBar = ({fetchWeather}) => {
     const [city, setCity] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (city.trim) {
+        if (city.trim()) {
             fetchWeather(city)
             setCity('');
         }
-    }
+    };
   return (
-    <form className='flex' onSubmit={handleSubmit} action="">
+    <form className='flex' onSubmit={handleSubmit}>
         <input type="text" placeholder='Enter city name' value={city} onChange={(e) => setCity(e.target.value)}
         className='flex-1 p-2 border border-r-0 border-gray-300 rounded-l-lg outline-none' />
         <button className='bg-blue-500 border cursor-pointer p-2 hover:bg-blue-600 rounded-r-lg' type='submit'>Search</button>
